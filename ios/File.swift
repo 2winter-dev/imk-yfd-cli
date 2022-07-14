0f15c3791b6d5e18d12db087ba72fc13e6137b54
ywt_oi <winter_986@qq.com>
2022-07-14 12:10:55 +0800

    支持远程仓库

--- ios/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  mstore
-//
-//  Created by Dang Tran on 4/25/20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
--- scripts/cebuilder.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd "/Users/apple/Documents/Works/InpriseUI/beoStore/mstore-expo"
-react-native run-ios
